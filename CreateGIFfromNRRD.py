1e880adb22362ef153e35ef81be31047f5af4705
Robert Court <robbie.court@gmail.com>
2013-08-28 20:02:39 +0100

    Creation of 3 plane combined view

--- CreateGIFfromNRRD.py
+++ CreateGIFfromNRRD.py
@@ -9,14 +9,32 @@ else:
 
     print 'Creating animated GIF of ', str(sys.argv[1]), ' and ', str(sys.argv[2]), '...'
   
-    readdata, options = nrrd.read(str(sys.argv[2]))
+    readdata, options = nrrd.read(str(sys.argv[1]))
     
     im1 = readdata
     
+    Tsize=im1.size
+    
+    imN = np.zeros([im1.shape[0]+im1.shape[2],im1.shape[1]+im1.shape[2],im1.shape[1],3],np.uint8)
+    
+    imC=imN[im1.shape[2]:,0:im1.shape[2],:,:]
+    
+    imS=imN[0:im1.shape[2],im1.shape[2]:,:,:]
+    
+    imT=imN[im1.shape[2]:,im1.shape[2]:,:,:]
+    
+    imC=np.array(np.reshape(np.repeat(np.rot90(im1,3),3),imC.shape))
+    
+    imT=np.array(np.repeat(np.reshape(np.repeat(im1,3),[imT.shape[0],imT.shape[1],im1.shape[2],imT.shape[3]]),np.int(np.ceil(float(im1.shape[1])/float(im1.shape[2]))),2))
+    
+    imS=np.array(np.repeat(np.reshape(np.repeat(np.transpose(np.flipud(im1),(2,1,0)),3),[imS.shape[0],imS.shape[1],im1.shape[0],imS.shape[3]]),np.int(np.ceil(float(im1.shape[0])/float(im1.shape[2]))),2))
+    
+    #TBA
+    
     readdata, options = nrrd.read(str(sys.argv[1])) 
-    im2 = readdata
+    im1 = readdata
       
-    if (im1.size <> im2.size):
+    if (Tsize <> im1.size):
         print '\n\nError: Images must be the same size!!'
     else:
     
