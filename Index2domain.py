e4f092f881809251b209af706cca71c6061c3987
Robert Court <robbie.court@gmail.com>
2013-09-03 17:40:31 +0100

    Levels taken down to 30

--- Index2domain.py
+++ Index2domain.py
@@ -20,7 +20,7 @@ else:
                     domain, options = nrrd.read(domfile)
                 else:
                     domain = np.zeros(readdata.shape,np.uint8)
-                domain[readdata==i]=np.uint8(55)
+                domain[readdata==i]=np.uint8(30)
                 nrrd.write(domfile, domain)
     
     
